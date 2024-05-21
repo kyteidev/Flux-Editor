fdfce7fec8a02af9c629d97ae5247f7334fed32e
kyteidev <89528285+kyteidev@users.noreply.github.com>
2024-05-21 22:05:36 +0300

    bug fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
 import App from "./App";
+import "./tailwind.css";
+import "./global.css";
 
 render(() => <App />, document.getElementById("root") as HTMLElement);
