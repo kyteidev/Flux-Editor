14f26cb02eb9b7a2b22f15b6584c5815f2bb0615
kyteidev <89528285+kyteidev@users.noreply.github.com>
2024-06-24 10:50:38 +0800

    fix css naming

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
 import App from "./App";
-import "./tailwind.css";
+import "./globals.css";
 
 render(() => <App />, document.getElementById("root") as HTMLElement);
