d432ed572b4c0b00db99f78007f91f6f21b789dd
kyteidev <89528285+kyteidev@users.noreply.github.com>
2024-06-24 13:48:03 +0800

    remove splash, app loads fast enough

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,5 @@
 import { render } from "solid-js/web";
 import App from "./App";
 import "./globals.css";
-import { invoke } from "@tauri-apps/api";
-
-document.addEventListener("DOMContentLoaded", () => {
-  invoke("close_splash");
-});
 
 render(() => <App />, document.getElementById("root") as HTMLElement);
