852f245ad196b0d9938aefd987e7df7bc981c8bf
kyteidev <89528285+kyteidev@users.noreply.github.com>
2024-07-09 15:50:45 +0800

    fix top level await

--- src/utils/os.ts
+++ src/utils/os.ts
@@ -17,7 +17,10 @@ You should have received a copy of the GNU General Public License along with Flu
 
 import { platform } from "@tauri-apps/api/os";
 
-const osName = (await platform()).toString();
+let osName: string;
+platform().then((platform) => {
+  osName = platform.toString();
+});
 
 export function getOS(): string {
   return osName;
