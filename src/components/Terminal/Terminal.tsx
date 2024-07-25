d69290a4bbe6eaec3dd0bd24e95bb189c97dcfb5
kyteidev <89528285+kyteidev@users.noreply.github.com>
2024-07-25 10:37:43 +0800

    add todo comment

--- src/components/Terminal/Terminal.tsx
+++ src/components/Terminal/Terminal.tsx
@@ -15,6 +15,8 @@ You should have received a copy of the GNU General Public License along with Flu
 <https://www.gnu.org/licenses/>.
 */
 
+// TODO: add real pty shell integration with Xterm
+
 import { onMount } from "solid-js";
 import { getProjectName, getProjectPath } from "../../App";
 import { invoke } from "@tauri-apps/api/tauri";
