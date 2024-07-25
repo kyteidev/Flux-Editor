d302c452ddddc208ebe64da61af7df38b3429b30
kyteidev <89528285+kyteidev@users.noreply.github.com>
2024-07-25 17:19:46 +0800

    feat: show error if update check failed

--- src/components/StatusBar/components/Update.tsx
+++ src/components/StatusBar/components/Update.tsx
@@ -15,9 +15,6 @@ You should have received a copy of the GNU General Public License along with Flu
 <https://www.gnu.org/licenses/>.
 */
 
-// the updater code is largely adapted from Tauri Docs: https://tauri.app/v1/guides/distribution/updater/#custom-dialog
-// the updater code from Tauri Docs is licensed under MIT. License notice can be found in resources/THIRD-PARTY-LICENSES-JS.txt
-
 import { relaunch } from "@tauri-apps/api/process";
 import {
   checkUpdate,
@@ -31,24 +28,38 @@ import { dialog } from "@tauri-apps/api";
 
 const Update = () => {
   const [os, setOs] = createSignal("");
-  const [hasUpdate, setHasUpdate] = createSignal(false);
+  const [show, setShow] = createSignal(true);
+
+  const [text, setText] = createSignal("");
+
+  let failed = false;
+  let errorMsg = "";
 
   const unlisten = onUpdaterEvent(({ error, status }) => {
-    info("Updater event: " + error + status);
+    info("Updater event: " + error + ", " + status);
+    if (error) {
+      setText("Failed to check updates");
+      failed = true;
+      errorMsg = error;
+    }
   });
 
   onMount(async () => {
     setOs(await getOS());
+    setText("Checking for updates");
 
     try {
       const { shouldUpdate } = await checkUpdate();
 
       if (shouldUpdate) {
-        setHasUpdate(true);
+        setShow(true);
 
         if (os() != "win32") {
-          installUpdate();
+          await installUpdate();
         }
+        setText("Click to apply update");
+      } else {
+        setShow(false);
       }
     } catch (e) {
       error(e as string);
@@ -60,26 +71,34 @@ const Update = () => {
     unlistenfn();
   });
 
-  const update = async () => {
-    const restart = await dialog.ask("Do you want to restart now?", {
-      title: "Restart required",
-    });
-    if (restart) {
-      if (os() === "win32") {
-        installUpdate();
-      } else {
-        relaunch();
+  const handleClick = async () => {
+    if (failed) {
+      setShow(false);
+      dialog.message(errorMsg, {
+        title: "Failed to check updates",
+        type: "error",
+      });
+    } else {
+      const restart = await dialog.ask("Do you want to restart now?", {
+        title: "Restart required",
+      });
+      if (restart) {
+        if (os() === "win32") {
+          installUpdate();
+        } else {
+          relaunch();
+        }
       }
     }
   };
 
   return (
-    <Show when={hasUpdate()}>
+    <Show when={show()}>
       <button
         class="flex items-center justify-center rounded bg-base-200 px-1 text-center hover:bg-base-100-hover active:brightness-125"
-        onClick={update}
+        onClick={handleClick}
       >
-        Click to apply update
+        {text()}
       </button>
     </Show>
   );
