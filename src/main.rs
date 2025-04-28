49a679c0aaf387da39107e859ba0d0cf241676df
kyteidev <89528285+kyteidev@users.noreply.github.com>
2025-04-28 20:18:58 +0300

    add hot reloading

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use freya::hotreload::FreyaCtx;
 use freya::prelude::*;
 
 fn main() {
+    dioxus_hot_reload::hot_reload_init!(Config::<FreyaCtx>::default());
+
     launch_cfg(
         app,
         LaunchConfig::<()>::builder()
@@ -10,7 +13,14 @@ fn main() {
 }
 
 fn app() -> Element {
-    rsx!(label {
-        "Flux Editor"
+    rsx!(rect {
+        width: "100%",
+        height: "100%",
+        background: "white",
+        border: "1px solid black",
+        padding: "10px",
+        label {
+            "Flux Editor"
+        }
     })
 }
